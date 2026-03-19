482c5358580d090047452b876a59df805454fab6
Trixie <tracyogbeta04@gmail.com>
2026-03-19 13:24:52 +0100

    Fixed database URL for deployment

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///C:/Projects.neuralwatch.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./neuralwatch.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
